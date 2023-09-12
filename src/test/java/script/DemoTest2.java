70dc1bbcd7940df9c800dfae0d2ad391b589b649
sabarisha <sabarisha@sabarisha>
2023-09-12 20:49:55 +0530

    Draft Copy

---
+++ src/test/java/script/DemoTest2.java
@@ -0,0 +1,20 @@
+package script;
+
+import static org.testng.Assert.fail;
+
+import org.testng.Assert;
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+import generic.BaseTest;
+
+public class DemoTest2 extends BaseTest
+{
+
+	@Test
+	public void testA()
+	{
+		Reporter.log(driver.getTitle(),true);
+		Assert.fail();
+	}
+}
