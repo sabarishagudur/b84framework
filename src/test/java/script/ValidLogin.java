fcc447749c85f009243f5c54ce9be7002bcb54ed
sabarisha <sabarisha@sabarisha>
2023-09-13 20:55:53 +0530

    added excel

--- src/test/java/script/ValidLogin.java
+++ src/test/java/script/ValidLogin.java
@@ -4,6 +4,7 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 
 import generic.BaseTest;
+import generic.Excel;
 import page.EnterTimeTrackPage;
 import page.LoginPage;
 
@@ -12,11 +13,13 @@ public class ValidLogin extends BaseTest
 	@Test(priority = 1,groups= {"smoke","regression"})
 	public void testValidLogin()
 	{
+		String un= Excel.getCellValue(xl_path, "validlogin", 1, 0);
+		String pwd=Excel.getCellValue(xl_path, "validlogin", 1, 1);
 //		1. enter valid un
 		LoginPage loginPage=new LoginPage(driver);
-		loginPage.setUserName("admin");
+		loginPage.setUserName(un);
 //		2. enter valid pw
-		loginPage.setPassword("manager");
+		loginPage.setPassword(pwd);
 //		3. click login button
 		loginPage.clickLoginButton();
 //		4. home page should be displayed
